c1a0608b8717304e94d133895670de19d26db844
Jim Fulton <jim@zope.com>
2004-02-21 12:28:55 +0000

    Moved permission declarations for persistent objects from Python to ZCML to prevent declarations, and thus, import of persistent types as a side effect of importing zope.app.  This is to avoid weird conflicts in FrankenZope installations, which currently run ZODB 3.2.

--- _protections.py
+++ _protections.py
@@ -13,92 +13,11 @@
 ##############################################################################
 """Register protection information for some standard low-level types
 
-$Id: _protections.py,v 1.9 2004/02/20 22:02:31 fdrake Exp $
+$Id: _protections.py,v 1.10 2004/02/21 12:28:55 jim Exp $
 """
 
 def protect():
     from zope.security.checker import defineChecker, NamesChecker, NoProxy
-
-
-    # excluding _check, _bucket_type, _firstbucket, and write operations
-    _btreeChecker = NamesChecker(['__str__', '__contains__',
-                                  '__getitem__', '__iter__', '__len__',
-                                  'byValue', 'get', 'has_key', 'items',
-                                  'iteritems', 'iterkeys', 'itervalues',
-                                  'keys', 'maxKey', 'minKey', 'values'])
-
-    # excluding _next
-    _btreeBucketChecker = NamesChecker([
-            '__contains__', '__getitem__', '__iter__', '__len__',
-            '__str__', 'byValue', 'get', 'has_key', 'items', 'iteritems',
-            'iterkeys', 'itervalues', 'keys', 'maxKey','minKey', 'values'])
-
-    _btreeSetChecker = NamesChecker([
-            '__contains__', '__getitem__', '__iter__', '__len__',
-            '__str__', 'has_key', 'insert', 'keys', 'maxKey', 'minKey'])
-
-    # excluding _bucket_type, _check
-    _btreeTreeSetChecker = NamesChecker([
-            '__contains__', '__iter__', '__len__',
-            '__str__', 'has_key', 'insert', 'keys', 'maxKey', 'minKey'])
-
-    _btreeItemsChecker = NamesChecker([
-            '__iter__', '__str__', '__getitem__', '__len__', '__contains__'])
-
-    _iteratorChecker = NamesChecker(['next'])
-
-    from BTrees.IIBTree import IIBTree, IIBucket, IISet, IITreeSet
-    from BTrees.IOBTree import IOBTree, IOBucket, IOSet, IOTreeSet
-    from BTrees.OIBTree import OIBTree, OIBucket, OISet, OITreeSet
-    from BTrees.OOBTree import OOBTree, OOBucket, OOSet, OOTreeSet
-
-    _btree_checkers = {
-        IIBTree: _btreeChecker,
-        IOBTree: _btreeChecker,
-        OIBTree: _btreeChecker,
-        OOBTree: _btreeChecker,
-        IIBucket: _btreeBucketChecker,
-        IOBucket: _btreeBucketChecker,
-        OIBucket: _btreeBucketChecker,
-        OOBucket: _btreeBucketChecker,
-        IISet: _btreeSetChecker,
-        IOSet: _btreeSetChecker,
-        OISet: _btreeSetChecker,
-        OOSet: _btreeSetChecker,
-        IITreeSet: _btreeTreeSetChecker,
-        IOTreeSet: _btreeTreeSetChecker,
-        OITreeSet: _btreeTreeSetChecker,
-        OOTreeSet: _btreeTreeSetChecker,
-        type(iter(IIBTree())): NoProxy, # II-iterator is a rock
-        type(iter(IOBTree())): _iteratorChecker, # IO-iterator
-        type(iter(OIBTree())): _iteratorChecker, # OI-iterator
-        type(iter(OOBTree())): _iteratorChecker, # OO-iterator
-        type(IIBTree().keys()): NoProxy, # IIBTreeItems is a rock
-        type(IOBTree().keys()): _btreeItemsChecker, # IOBTreeItems
-        type(OIBTree().keys()): _btreeItemsChecker, # OIBTreeItems
-        type(OOBTree().keys()): _btreeItemsChecker, # OOBTreeItems
-    }
-    for which_type, checker in _btree_checkers.iteritems():
-        defineChecker(which_type, checker)
-
-    from persistent.list import PersistentList
-
-    defineChecker(PersistentList,
-                  NamesChecker(
-                     ['__getitem__', '__getslice__', '__len__', '__iter__',
-                      '__contains__', 'index', 'count'])
-                  )
-
-    from persistent.dict import PersistentDict
-
-    defineChecker(PersistentDict,
-                  NamesChecker(['__getitem__', '__len__', '__iter__',
-                        'get', 'has_key', '__copy__',
-                        'keys', 'values', 'items',
-                        'iterkeys', 'iteritems', 'itervalues', '__contains__',
-                        ]
-                     )
-                  )
 
     # Make sure the message id gets never proxied
     from zope.i18n.messageid import MessageID
