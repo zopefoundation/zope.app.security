85e413d7f3b61f996754aa6c4f6c71963acf86f5
Sidnei da Silva <sidnei.da.silva@gmail.com>
2004-07-09 20:09:14 +0000

    Add __traceback_info__ to zope.app.traversing.adapters, as it seems its a very common place to have errors when you mistype something on a view template, and no info was provided that could help finding the source of the problem. Fix a typo and clarify by adding a comment the code inside protectclass.

--- protectclass.py
+++ protectclass.py
@@ -34,7 +34,7 @@ def protectName(class_, name, permission):
         # Translate public permission to CheckerPublic
         permission = CheckerPublic
 
-    # We know a dictionart get method was used because we set it
+    # We know a dictionary was used because we set it
     protections = checker.get_permissions
     protections[name] = permission
 
@@ -50,7 +50,13 @@ def protectSetAttribute(class_, name, permission):
         # Translate public permission to CheckerPublic
         permission = CheckerPublic
 
-    # We know a dictionart get method was used because we set it
+    # We know a dictionary was used because we set it
+    # Note however, that if a checker was created manually
+    # and the caller used say NamesChecker or MultiChecker,
+    # then set_permissions may be None here as Checker
+    # defaults a missing set_permissions parameter to None.
+    # Jim says this doensn't happens with the C version of the
+    # checkers because they use a 'shared dummy dict'.
     protections = checker.set_permissions
     protections[name] = permission
 
@@ -61,7 +67,13 @@ def protectLikeUnto(class_, like_unto):
     if unto_checker is None:
         return
 
-    # We know a dictionart get method was used because we set it
+    # We know a dictionary was used because we set it
+    # Note however, that if a checker was created manually
+    # and the caller used say NamesChecker or MultiChecker,
+    # then set_permissions may be None here as Checker
+    # defaults a missing set_permissions parameter to None.
+    # Jim says this doensn't happens with the C version of the
+    # checkers because they use a 'shared dummy dict'.
     unto_get_protections = unto_checker.get_permissions
     unto_set_protections = unto_checker.set_permissions
 
@@ -70,7 +82,6 @@ def protectLikeUnto(class_, like_unto):
         checker = Checker({}, {})
         defineChecker(class_, checker)
 
-    # OK, so it's a hack.
     get_protections = checker.get_permissions
     for name in unto_get_protections:
         get_protections[name] = unto_get_protections[name]
