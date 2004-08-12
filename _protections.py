a89623bac6ef7c5e9b76f699d81ebd4c4fd0a245
Stephan Richter <stephan.richter@gmail.com>
2004-08-12 16:16:03 +0000

    Backported
    r26480 | srichter | 2004-07-13 13:03:22 -0400 (Tue, 13 Jul 2004) | 2 lines
    r26481 | srichter | 2004-07-13 13:04:09 -0400 (Tue, 13 Jul 2004) | 3 lines
    r26482 | srichter | 2004-07-13 13:07:03 -0400 (Tue, 13 Jul 2004) | 2 lines
    r26485 | srichter | 2004-07-13 13:11:43 -0400 (Tue, 13 Jul 2004) | 2 lines
    r26486 | srichter | 2004-07-13 13:12:34 -0400 (Tue, 13 Jul 2004) | 3 lines

---
+++ _protections.py
@@ -0,0 +1,30 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+"""Register protection information for some standard low-level types
+
+$Id$
+"""
+
+def protect():
+    from zope.security.checker import defineChecker, NamesChecker, NoProxy
+
+    # Make sure the message id gets never proxied
+    from zope.i18n.messageid import MessageID
+    defineChecker(MessageID, NoProxy)
+
+    # add __parent__ and __name__ to always available names
+    import zope.security.checker
+    for name in ['__name__', '__parent__']:
+        if name not in zope.security.checker._available_by_default:
+            zope.security.checker._available_by_default.append(name)
