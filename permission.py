c99f1a0b231a7a8e4fc8ff2b7430f55c1440d64e
Stephan Richter <stephan.richter@gmail.com>
2004-12-05 21:08:25 +0000

    Implemented local permissions.

--- permission.py
+++ permission.py
@@ -15,12 +15,16 @@
 
 $Id$
 """
+from persistent import Persistent
 from zope.interface import implements
 from zope.schema.interfaces import ValidationError
 from zope.security.checker import CheckerPublic
 from zope.app import zapi
+from zope.app.location import Location
 from zope.app.security.interfaces import IPermission
 
+from zope.app.i18n import ZopeMessageIDFactory as _
+NULL_ID = _('<permission not activated>')
 
 class Permission(object):
     implements(IPermission)
@@ -29,6 +33,106 @@ class Permission(object):
         self.id = id
         self.title = title
         self.description = description
+
+
+class LocalPermission(Persistent, Location):
+    implements(IPermission)
+
+    def __init__(self, title="", description=""):
+        self.id = NULL_ID
+        self.title = title
+        self.description = description
+
+
+def setIdOnActivation(event):
+    """Set the permission id upon registration activation.
+
+    Let's see how this notifier can be used. First we need to create an event
+    using the permission instance and a registration stub:
+
+    >>> class Registration:
+    ...     def __init__(self, obj, name):
+    ...         self.object = obj
+    ...         self.name = name
+    ...
+    ...     def getComponent(self):
+    ...         return self.object
+
+    >>> perm1 = LocalPermission('Permission 1', 'A first permission')
+    >>> perm1.id
+    u'<permission not activated>'
+    
+    >>> from zope.app.registration import registration 
+    >>> event = registration.RegistrationActivatedEvent(
+    ...     Registration(perm1, 'perm1'))
+
+    Now we pass the event into this function, and the id of the permission
+    should be set to 'perm1'.
+
+    >>> setIdOnActivation(event)
+    >>> perm1.id
+    'perm1'
+
+    If the function is called and the component is not a local permission,
+    nothing is done:
+
+    >>> class Foo:
+    ...     id = 'no id'
+    >>> foo = Foo()
+    >>> event = registration.RegistrationActivatedEvent(
+    ...     Registration(foo, 'foo'))
+    >>> setIdOnActivation(event)
+    >>> foo.id
+    'no id'
+    """
+    perm = event.object.getComponent()
+    if isinstance(perm, LocalPermission):
+        perm.id = event.object.name
+
+
+def unsetIdOnDeactivation(event):
+    """Unset the permission id up registration deactivation.
+
+    Let's see how this notifier can be used. First we need to create an event
+    using the permission instance and a registration stub:
+
+    >>> class Registration:
+    ...     def __init__(self, obj, name):
+    ...         self.object = obj
+    ...         self.name = name
+    ...
+    ...     def getComponent(self):
+    ...         return self.object
+
+    >>> perm1 = LocalPermission('Permission 1', 'A first permission')
+    >>> perm1.id = 'perm1'
+
+    >>> from zope.app.registration import registration 
+    >>> event = registration.RegistrationDeactivatedEvent(
+    ...     Registration(perm1, 'perm1'))
+
+    Now we pass the event into this function, and the id of the permission
+    should be set to NULL_ID.
+
+    >>> unsetIdOnDeactivation(event)
+    >>> perm1.id
+    u'<permission not activated>'
+
+    If the function is called and the component is not a local permission,
+    nothing is done:
+
+    >>> class Foo:
+    ...     id = 'foo'
+    >>> foo = Foo()
+    >>> event = registration.RegistrationDeactivatedEvent(
+    ...     Registration(foo, 'foo'))
+    >>> unsetIdOnDeactivation(event)
+    >>> foo.id
+    'foo'
+    """
+    perm = event.object.getComponent()
+    if isinstance(perm, LocalPermission):
+        perm.id = NULL_ID
 
 
 def checkPermission(context, permission_id):
