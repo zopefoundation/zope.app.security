6a54e8b2afe0da1de3112e8346c1d7ff0d20a1f0
Gary Poster <gary@zope.com>
2005-04-15 16:11:46 +0000

    Configure zope.i18nmessageid.Message to be a brick.  Add comments and a TODO reminding that MessageID's brick status is problematic.

--- _protections.py
+++ _protections.py
@@ -21,7 +21,13 @@ def protect():
 
     # Make sure the message id gets never proxied
     from zope.i18n.messageid import MessageID
+    from zope.i18nmessageid import Message
+    # TODO because MessageIDs are mutable, this is a security hole.  This hole
+    # is one of the primary reasons for the development of the Message 
+    # replacement.  See zope/i18nmessageid/messages.txt.
     defineChecker(MessageID, NoProxy)
+    # this, however, is not a security hole, because Messages are immutable.
+    defineChecker(Message, NoProxy)
 
     # add __parent__ and __name__ to always available names
     import zope.security.checker
