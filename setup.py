1f1a0a1392c2fccb521baad34fa87027a70e3240
Stephan Richter <stephan.richter@gmail.com>
2007-10-27 20:42:22 +0000

    Get ready for release.

--- setup.py
+++ setup.py
@@ -15,21 +15,54 @@
 
 $Id$
 """
-
 import os
+from setuptools import setup, find_packages
 
-from setuptools import setup, find_packages, Extension
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.security',
-      version = '3.4.0b1',
-      url='http://svn.zope.org/zope.app.security',
-      license='ZPL 2.1',
+      version = '3.4.0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
-	  packages=find_packages('src'),
-	  package_dir = {'': 'src'},
-      extras_require=dict(test=['zope.app.testing']),
+      description='Security Components for Zope 3 Applications',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          'Detailed Dcoumentation\n' +
+          '======================\n'
+          + '\n\n' +
+          read('src', 'zope', 'app', 'security', 'globalprincipals.txt')
+          + '\n\n' +
+          read('src', 'zope', 'app', 'security', 'logout.txt')
+          + '\n\n' +
+          read('src', 'zope', 'app', 'security', 'browser',
+               'authutilitysearchview.txt')
+          + '\n\n' +
+          read('src', 'zope', 'app', 'security', 'browser', 'loginlogout.txt')
+          + '\n\n' +
+          read('src', 'zope', 'app', 'security', 'browser',
+               'principalterms.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 security authentication principal ftp http",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.app.security',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
       namespace_packages=['zope', 'zope.app'],
+      extras_require=dict(test=['zope.app.testing']),
       install_requires=['setuptools',
                         'zope.app.authentication',
                         'zope.app.component',
@@ -51,6 +84,5 @@ setup(name='zope.app.security',
                         'ZODB3',
                         ],
       include_package_data = True,
-
       zip_safe = False,
       )
