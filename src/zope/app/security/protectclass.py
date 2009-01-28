8a5ec1c0fb16128775fd01a3f71a64da3dad9d9f
Brandon Rhodes <brandon@rhodesmill.org>
2009-01-28 13:16:45 +0000

    Removed the `protectclass` module from zope.app.security (leaving in place a compatibility module that imports the functions from the new location) so that packages that need them can remove a this package as a dependency.

--- src/zope/app/security/protectclass.py
+++ src/zope/app/security/protectclass.py
@@ -11,79 +11,13 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""Make assertions about permissions needed to access class instances
-attributes
+"""Backward compatibility: code was moved to zope.security.protectclass.
 
 $Id$
 """
 from zope.security.checker import defineChecker, getCheckerForInstancesOf
 from zope.security.checker import Checker, CheckerPublic
 
-
-def protectName(class_, name, permission):
-    """Set a permission on a particular name."""
-
-    checker = getCheckerForInstancesOf(class_)
-    if checker is None:
-        checker = Checker({}, {})
-        defineChecker(class_, checker)
-
-    if permission == 'zope.Public':
-        # Translate public permission to CheckerPublic
-        permission = CheckerPublic
-
-    # We know a dictionary was used because we set it
-    protections = checker.get_permissions
-    protections[name] = permission
-
-def protectSetAttribute(class_, name, permission):
-    """Set a permission on a particular name."""
-
-    checker = getCheckerForInstancesOf(class_)
-    if checker is None:
-        checker = Checker({}, {})
-        defineChecker(class_, checker)
-
-    if permission == 'zope.Public':
-        # Translate public permission to CheckerPublic
-        permission = CheckerPublic
-
-    # We know a dictionary was used because we set it
-    # Note however, that if a checker was created manually
-    # and the caller used say NamesChecker or MultiChecker,
-    # then set_permissions may be None here as Checker
-    # defaults a missing set_permissions parameter to None.
-    # Jim says this doensn't happens with the C version of the
-    # checkers because they use a 'shared dummy dict'.
-    protections = checker.set_permissions
-    protections[name] = permission
-
-def protectLikeUnto(class_, like_unto):
-    """Use the protections from like_unto for class_"""
-
-    unto_checker = getCheckerForInstancesOf(like_unto)
-    if unto_checker is None:
-        return
-
-    # We know a dictionary was used because we set it
-    # Note however, that if a checker was created manually
-    # and the caller used say NamesChecker or MultiChecker,
-    # then set_permissions may be None here as Checker
-    # defaults a missing set_permissions parameter to None.
-    # Jim says this doensn't happens with the C version of the
-    # checkers because they use a 'shared dummy dict'.
-    unto_get_protections = unto_checker.get_permissions
-    unto_set_protections = unto_checker.set_permissions
-
-    checker = getCheckerForInstancesOf(class_)
-    if checker is None:
-        checker = Checker({}, {})
-        defineChecker(class_, checker)
-
-    get_protections = checker.get_permissions
-    for name in unto_get_protections:
-        get_protections[name] = unto_get_protections[name]
-
-    set_protections = checker.set_permissions
-    for name in unto_set_protections:
-        set_protections[name] = unto_set_protections[name]
+from zope.security.protectclass import (protectName,
+                                        protectSetAttribute,
+                                        protectLikeUnto)
