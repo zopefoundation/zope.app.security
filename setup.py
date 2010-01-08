4b4b73c912a38c22adff5a2a362305f07a0780a1
Martijn Faassen <faassen@startifact.com>
2010-01-08 22:04:43 +0000

    Preparing release 3.7.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
 
-version = '3.7.5dev'
+version = '3.7.5'
 
 import os
 from setuptools import setup, find_packages
